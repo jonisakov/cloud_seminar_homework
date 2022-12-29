4ae9d31e5c9a498025c5a115e7f8a48128ecf1aa
jonisakov <jonisakov@users.noreply.github.com>
2022-12-29 18:44:13 +0200

    Update recive_http_post.py

--- recive_http_post.py
+++ recive_http_post.py
@@ -3,42 +3,51 @@ import json
 import logging
 from fdk import response
 import oci
-      
-def handler(ctx, data: io.BytesIO=None):
+import time
+
+
+def detect_request_type(ctx):
+    if ctx.method == 'GET':
+        return 'GET'
+    elif ctx.method == 'POST':
+        return 'POST'
+
+
+def handler(ctx, data: io.BytesIO = None):
     if None == ctx.RequestURL():
         return "Function loaded properly but not invoked via an HTTP request."
     signer = oci.auth.signers.get_resource_principals_signer()
-    logging.getLogger().info("URI: " + ctx.RequestURL() )
+    logging.getLogger().info("URI: " + ctx.RequestURL())
     config = {
         # update with your tenancy's OCID
         "tenancy": "ocid1.tenancy.oc1..aaaaaaaat3g6mubuxwcl26ef5tve3gpoz3bnrueskq7ma2fyjlk3jiiinxea",
         # replace with the region you are using
         "region": "il-jerusalem-1."
     }
-    if(ctx.method == 'GET'):
-      try:
-          object_storage = oci.object_storage.ObjectStorageClient(config, signer=signer)
-          namespace = object_storage.get_namespace().data
-          # update with your bucket name
-          bucket_name = "cloud_seminar_homework"
-          file_object_name = ctx.RequestURL()
-          if file_object_name.endswith("/"):
-              logging.getLogger().info("Adding index.html to reques URL " + file_object_name)
-              file_object_name += "index.html"
+    if (detect_request_type(ctx) == 'GET'):
+        try:
+            object_storage = oci.object_storage.ObjectStorageClient(config, signer=signer)
+            namespace = object_storage.get_namespace().data
+            # update with your bucket name
+            bucket_name = "cloud_seminar_homework"
+            file_object_name = ctx.RequestURL()
+            if file_object_name.endswith("/"):
+                logging.getLogger().info("Adding index.html to reques URL " + file_object_name)
+                file_object_name += "index.html"
 
-          # strip off the first character of the URI (i.e. the /)
-          file_object_name = file_object_name[1:]
+            # strip off the first character of the URI (i.e. the /)
+            file_object_name = file_object_name[1:]
 
-          obj = object_storage.get_object(namespace, bucket_name, file_object_name)
-          return response.Response(
-              ctx, response_data=obj.data.content,
-              headers={"Content-Type": obj.headers['Content-type']}
-          )
-      except (Exception) as e:
-          return response.Response(
-              ctx, response_data="500 Server error- GET",
-              headers={"Content-Type": "text/plain"}
-              )
+            obj = object_storage.get_object(namespace, bucket_name, file_object_name)
+            return response.Response(
+                ctx, response_data=obj.data.content,
+                headers={"Content-Type": obj.headers['Content-type']}
+            )
+        except (Exception) as e:
+            return response.Response(
+                ctx, response_data="500 Server error- GET",
+                headers={"Content-Type": "text/plain"}
+            )
     else:
         try:
             request_body = request.get_json()
@@ -46,7 +55,7 @@ def handler(ctx, data: io.BytesIO=None):
             object_storage_client = oci.object_storage.ObjectStorageClient()
             # create a unique object name for the request body
             object_name = 'request_body_' + str(int(time.time())) + '.json'
-            
+
             # write the JSON to an object in the bucket
             object_storage_client.put_object(
                 bucket_name='cloud_seminar_homework',
@@ -54,13 +63,12 @@ def handler(ctx, data: io.BytesIO=None):
                 content_type='application/json',
                 body=json.dumps(request_body)
             )
-            
+
             # return a success response
             return {'status': 'success', 'object_name': object_name}
-      except (Exception) as e:
-          return response.Response(
-              ctx, response_data="500 Server error- POST",
-              headers={"Content-Type": "text/plain"}
-              )
-        
+        except (Exception) as e:
+            return response.Response(
+            ctx, response_data="500 Server error- POST",
+            headers={"Content-Type": "text/plain"}
+        )
 
